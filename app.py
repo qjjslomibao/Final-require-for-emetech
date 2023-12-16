6a57ee6104f42f4fc4c6d10a3b5edb8cd77a617a
qjjslomibao <112042148+qjjslomibao@users.noreply.github.com>
2023-12-16 13:40:12 +0800

    Update app.py

--- app.py
+++ app.py
@@ -10,34 +10,30 @@
 # Date Submitted: December 11, 2023
 import streamlit as st
 import tensorflow as tf
-
-@st.cache_resource
-def load_model():
-  model = tf.keras.models.load_model('best_model.h5')
-
-  return model
-model=load_model()
-st.write("""
-# Weather Classification"""
-)
-file=st.file_uploader("Upload a weather photo from your computer.",type=["jpg","png"])
-
-import cv2
-from PIL import Image,ImageOps
+from PIL import Image
 import numpy as np
-def import_and_predict(image_data,model):
-    size=(60,60)
-    image=ImageOps.fit(image_data,size,Image.ANTIALIAS)
-    img=np.asarray(image)
-    img_reshape=img[np.newaxis,...]
-    prediction=model.predict(img_reshape)
-    return prediction
-if file is None:
-    st.text("Please upload an image related to weather.")
-else:
-    image=Image.open(file)
-    st.image(image,use_column_width=True)
-    prediction=import_and_predict(image,model)
-    class_names = ['Cloudy', 'Rain', 'Shine', 'Sunrise']
-    string="OUTPUT : "+class_names[np.argmax(prediction)]
-    st.success(string)
+
+@st.cache(allow_output_mutation=True)
+def load_model():
+    model = tf.keras.models.load_model('best_model.h5')
+    return model
+
+model = load_model()
+
+st.title("Emtech2 - Emotion Prediction App")
+
+uploaded_file = st.file_uploader("Choose an image...", type="jpg")
+
+if uploaded_file is not None:
+    img = Image.open(uploaded_file)
+    img = img.resize((64, 64))
+    img_array = np.array(img)
+    img_array = img_array / 255.0
+    img_array = np.expand_dims(img_array, axis=0)
+
+    prediction = model.predict(img_array)
+    predicted_class = "Happy" if prediction[0, 0] >= 0.5 else "Sad"
+    confidence = prediction[0, 0] if predicted_class == "Happy" else 1 - prediction[0, 0]
+    confidence_scalar = float(confidence)
+
+    st.image(img, caption=f'Predicted Class: {predicted_class} (Confidence: {confidence_scalar:.2f})', use_column_width=True)
