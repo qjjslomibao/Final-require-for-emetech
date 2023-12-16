ede648b57ff4b5b7cb4bf85b5998d2e38e81a163
qjjslomibao <112042148+qjjslomibao@users.noreply.github.com>
2023-12-16 12:06:19 +0800

    app.py

--- app.py
+++ app.py
@@ -30,12 +30,14 @@ def install_packages():
         import tensorflow
     except ImportError:
         st.warning("Installing TensorFlow...")
-        st.code("!pip install tensorflow", language="bash")
+        st.code("!pip install tensorflow==2.7.0", language="bash")
         st.success("TensorFlow installed!")
-        
+
 install_packages()
+
 from tensorflow.keras.models import load_model
 
+# Load the trained model
 model_path = '/best_model.h5'
 model = load_model(model_path)
 
@@ -44,7 +46,7 @@ st.title("Emtech2 - Emotion Prediction App")
 uploaded_file = st.file_uploader("Choose an image...", type="jpg")
 
 if uploaded_file is not None:
- 
+
     img = Image.open(uploaded_file)
     img = img.resize((64, 64))
     img_array = image.img_to_array(img)
@@ -56,7 +58,5 @@ if uploaded_file is not None:
     predicted_class = "Happy" if prediction[0] >= 0.5 else "Sad"
     confidence = prediction[0] if predicted_class == "Happy" else 1 - prediction[0]
     confidence_scalar = float(confidence)
-
-    st.image(img, caption=f'Predicted Class: {predicted_class} (Confidence: {confidence_scalar:.2f})', use_column_width=True)
 
     st.image(img, caption=f'Predicted Class: {predicted_class} (Confidence: {confidence_scalar:.2f})', use_column_width=True)
