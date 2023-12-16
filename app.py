b1dd7e62a251d76a8a2dc334af60b74b3b9b017f
qjjslomibao <112042148+qjjslomibao@users.noreply.github.com>
2023-12-16 13:53:29 +0800

    Update app.py

--- app.py
+++ app.py
@@ -9,29 +9,13 @@
 # Date Perform: December 10, 2023
 # Date Submitted: December 11, 2023
 import streamlit as st
-import torch
-import torch.nn as nn
-from torchvision import transforms
+import tensorflow as tf
 from PIL import Image
 import numpy as np
 
-# Define a simple neural network for emotion prediction
-class EmotionModel(nn.Module):
-    def __init__(self):
-        super(EmotionModel, self).__init__()
-        # Modify this architecture based on your specific requirements
-        self.fc = nn.Linear(64 * 64 * 3, 1)
-
-    def forward(self, x):
-        x = x.view(x.size(0), -1)
-        x = torch.sigmoid(self.fc(x))
-        return x
-
-# Load the pre-trained PyTorch model
 @st.cache(allow_output_mutation=True)
 def load_model():
-    model = EmotionModel()
-    model.load_state_dict(torch.load('best_model.pth', map_location=torch.device('cpu')))
+    model = tf.keras.models.load_model('best_model.h5')
     return model
 
 model = load_model()
@@ -45,13 +29,11 @@ if uploaded_file is not None:
     img = img.resize((64, 64))
     img_array = np.array(img)
     img_array = img_array / 255.0
-    img_tensor = torch.from_numpy(np.transpose(img_array, (2, 0, 1))).float()
-    img_tensor = img_tensor.unsqueeze(0)
+    img_array = np.expand_dims(img_array, axis=0)
 
-    # Forward pass through the PyTorch model
-    prediction = model(img_tensor)
-    predicted_class = "Happy" if prediction.item() >= 0.5 else "Sad"
-    confidence = prediction.item() if predicted_class == "Happy" else 1 - prediction.item()
+    prediction = model.predict(img_array)
+    predicted_class = "Happy" if prediction[0, 0] >= 0.5 else "Sad"
+    confidence = prediction[0, 0] if predicted_class == "Happy" else 1 - prediction[0, 0]
     confidence_scalar = float(confidence)
 
     st.image(img, caption=f'Predicted Class: {predicted_class} (Confidence: {confidence_scalar:.2f})', use_column_width=True)
